44dc7e2750a8f15612a3e51be513e97d6340d24a
vgemertb <beau.vangemert@vito.be>
2026-02-20 14:58:23 +0100

    [WA-475] Create Web ID creation API [WA-476] Create Add Pod Location To Web ID API [WA-504] Provide web id service
    - Provide means to add additional scopes to ID token

--- src/service/token-service.ts
+++ src/service/token-service.ts
@@ -2,6 +2,7 @@ import { JWK } from "jose";
 import log from "loglevel";
 import {createDpop} from "../factory/token-factory";
 import {Service} from "./service";
+import {isRunningInDocker} from "@we-are-components/shared";
 
 /**
  * Service class for handling token requests.
@@ -11,15 +12,17 @@ export class TokenService extends Service {
     /**
      * Requests an access token.
      * @param {string} [dpopHeader] - Optional DPoP header.
+     * @param scopes
      * @returns {Promise<any>} A promise that resolves to the token response.
      */
-    async requestAccessToken(dpopHeader?: string): Promise<any> {
+    async requestAccessToken(dpopHeader?: string, scopes = ""): Promise<any> {
         if(!this.oidcConfig) throw Error("[TokenService.requestAccessToken] OIDC configuration is required to request access token from We Are OIDC.");
 
         const body = new URLSearchParams({
             grant_type: 'client_credentials',
             client_id: this.oidcConfig.clientId,
-            client_secret: this.oidcConfig.clientSecret
+            client_secret: this.oidcConfig.clientSecret,
+            scopes: scopes
         });
 
         const extraHeaders = {} as { dpop?: string };
@@ -27,7 +30,7 @@ export class TokenService extends Service {
             extraHeaders.dpop = dpopHeader;
         }
 
-        log.debug(`[requestAccessToken] Requesting access token from ${this.oidcConfig.tokenEndpoint}`);
+        log.debug(`[requestAccessToken] Requesting access token from ${this.oidcConfig.tokenEndpoint!}`);
         const response = await fetch(this.oidcConfig.tokenEndpoint!, {
             method: 'POST',
             headers: {
